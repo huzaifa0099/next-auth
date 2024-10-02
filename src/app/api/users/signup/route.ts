f578ce277215599e7dda0053fbf16950b538c85a
Huzaifa Ali <huzaifaali006@gmail.com>
2024-10-02 07:47:08 -0700

    connect mailerTrap in mailer

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,26 +1,29 @@
-import {connect} from "@/dbConfig/dbConfig"
-import User from '@/models/userModel'
-import { NextRequest, NextResponse } from "next/server"
-import bcryptjs from "bcryptjs"
-import { log } from "console"
-import { sendEmail } from "@/helpers/mailer"
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
 
 connect()
 
-export async function POST(request: NextRequest) {
+
+export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json()
         const {username, email, password} = reqBody
 
-        console.log(reqBody)
+        console.log(reqBody);
 
+        //check if user already exists
         const user = await User.findOne({email})
 
         if(user){
             return NextResponse.json({error: "User already exists"}, {status: 400})
         }
 
-        const salt = await bcryptjs.genSalt()
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password, salt)
 
         const newUser = new User({
@@ -32,21 +35,21 @@ export async function POST(request: NextRequest) {
         const savedUser = await newUser.save()
         console.log(savedUser);
 
-        // send verification email
+        //send verification email
 
         await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
-            message: "User registered successfully!",
+            message: "User created successfully",
             success: true,
-            savedUser,
-
+            savedUser
         })
-
-
         
         
+
+
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
+
     }
 }
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,32 +1,49 @@
+import User from "@/models/userModel";
 import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
-
-  
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
-        user: "maddison53@ethereal.email",
-        pass: "jn7jnAPss4f63QBp6D",
+        user: "bee64782e25780",
+        pass: "********93da",
       },
     });
 
     const mailOptions = {
-      from: "hitesh@hitesh.ai",
+      from: "hitesh@gmail.com",
       to: email,
       subject:
-        emailType === "VERIFY" ? "Verify your email" : "Reset your Password",
-
-      html: "<b>Hello world?</b>",
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+            or copy and paste the link below in your browser. <br> ${
+              process.env.DOMAIN
+            }/verifyemail?token=${hashedToken}
+            </p>`,
     };
-
-    const mailResponse = await transporter.sendMail(mailOptions)
-    return mailResponse
-
-  } catch (error:any) {
-    throw new Error(error.message)
+    const mailResponse = await transport.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
   }
 };
