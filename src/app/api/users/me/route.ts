9449c1bde146e8d89a2a9a83d5a9969794fde985
Huzaifa Ali <huzaifaali006@gmail.com>
2024-10-08 19:01:22 -0700

    add user route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,16 +1,20 @@
-import { connect } from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
-import { NextRequest, NextResponse } from "next/server";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
-connect()
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
 
-export async function POST(request:NextRequest) {
-    const userId = await getDataFromToken(request)
-    const user = await User.findOne({_id: userId}).select("-password")
-    
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
     return NextResponse.json({
-        message: "User found",
-        data: user
-    })
+      mesaaage: "User found",
+      data: user,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
 }
