229eafde22d8d1a1fb68994b783d80836704fa38
Huzaifa Ali <huzaifaali006@gmail.com>
2024-10-08 19:00:37 -0700

    update userModel

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,36 +1,34 @@
 import mongoose from "mongoose";
 
-import { type } from "os";
-
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "please provide a username"],
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "please provide an email"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "please provide a password"],
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  forgotPasswordToken: String,
-  forgotPasswordTokenExpiry: Date,
-  verifyToken: String,
-  verifyTokenExpiry: Date,
-});
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerfied: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
 
-const User = mongoose.model.users || mongoose.model("users", userSchema)
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
 export default User;
